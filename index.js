60f51e03e4385aebbe4920f2ce5df12469b6bc83
Matti Riekkinen <matti.riekkinen@icloud.com>
2018-10-12 17:50:07 +0300

    Tehtävät 3.1 - 3.6

---
+++ index.js
@@ -0,0 +1,105 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Martti Tienari",
+    number: "040-123456",
+    id: 2
+  },
+  {
+    name: "Arto Järvinen",
+    number: "040-123456",
+    id: 3
+  },
+  {
+    name: "Lea Kutvonen",
+    number: "040-123456",
+    id: 4
+  }
+]
+
+app.get('/info', (request, response) => {
+  response.send(
+    `<div>
+      puhelinluettelossa ${persons.length} henkilön tiedot
+    </div>
+    <br/>
+    <div>
+      ${new Date()}
+    </div>`
+    )
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+
+  if (!person) {
+    return response.status(404).end()
+  }
+
+  response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(p => p.id !== id)
+
+  response.status(204).end()
+})
+
+function findName(name) {
+  return persons.find(p => p.name === name)
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function generateId() {
+  return getRandomInt(1000000)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (body.name === undefined) {
+    return response.status(400).json({ error: 'name missing' })
+  }
+
+  if (body.number === undefined) {
+    return response.status(400).json({ error: 'number missing' })
+  }
+
+  if (findName(body.name) !== undefined) {
+    return response.status(400).json({ error: 'name must be unique' })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons.push(person)
+
+  response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
